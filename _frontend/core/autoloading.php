36af49e8cdf6e72bb7437615974bfc3fabb98dce
Tyrone <tyrone@clearmindai.com>
2025-09-02 07:29:36 +0800

    include error and template page

--- _frontend/core/autoloading.php
+++ _frontend/core/autoloading.php
@@ -10,9 +10,9 @@
  */
 
 if (! function_exists("autoload_php")) {
-    function autoload_php(string|array $filename=null)
+    function autoload_php(string|array $filename = null)
     {
-        if(!$filename){
+        if (!$filename) {
             return false;
         }
         if (is_array($filename)) {
@@ -27,66 +27,112 @@ if (! function_exists("autoload_php")) {
     }
 }
 
-if(! function_exists("use_library")){
-    function use_library(string $library){
-        $model = substr($library, -4)==".php" ? $library : $library.".php";
-        include "_frontend/core/library/".$model;
+
+if (! function_exists("include_error_page")) {
+    function include_error_page(string|null $error_page, $variables = [], $exit = true)
+    {
+        $error_page = substr($error_page, -4) == ".php" ? $error_page : $error_page . ".php";
+        $path = "_frontend/extra/errors/$error_page";
+        if (! file_exists($path)) {
+            die("Error page '$error_page' not found.!");
+        }
+        if (!empty($variables)) {
+            extract($variables);
+        }
+        include($path);
+        if ($exit) {
+            exit;
+        }
     }
 }
 
-if(! function_exists("import_swal")){
-    function import_swal(){
-        ?>
-        <script src="<?=assets('code/swal.js')?>"></script>
-        <?php
+if (! function_exists("include_template_page")) {
+    function include_template_page(string|null $template_page, $variables = [], $exit = true)
+    {
+        $error_page = $template_page;
+        $error_page = substr($error_page, -4) == ".php" ? $error_page : $error_page . ".php";
+        $path = "_frontend/extra/template/$error_page";
+        if (! file_exists($path)) {
+            die("Template page '$error_page' not found.!");
+        }
+        if (!empty($variables)) {
+            extract($variables);
+        }
+        include($path);
+        if ($exit) {
+            exit;
+        }
     }
 }
 
-if(! function_exists("import_twal")){
-    function import_twal(){
-        ?>
-        <script src="<?=assets('code/twal.js')?>"></script>
-        <?php
+if (! function_exists("use_library")) {
+    function use_library(string $library)
+    {
+        $model = substr($library, -4) == ".php" ? $library : $library . ".php";
+        include "_frontend/core/library/" . $model;
     }
 }
 
-if(! function_exists("import_jquery")){
-    function import_jquery(){
-        ?>
-        <script src="<?=assets('code/jquery.js')?>"></script>
-        <?php
+if (! function_exists("import_swal")) {
+    function import_swal()
+    {
+?>
+        <script src="<?= assets('code/swal.js') ?>"></script>
+    <?php
     }
 }
 
-if(! function_exists("import_tyrux")){
-    function import_tyrux(){
+if (! function_exists("import_twal")) {
+    function import_twal()
+    {
+    ?>
+        <script src="<?= assets('code/twal.js') ?>"></script>
+    <?php
+    }
+}
+
+if (! function_exists("import_jquery")) {
+    function import_jquery()
+    {
+    ?>
+        <script src="<?= assets('code/jquery.js') ?>"></script>
+    <?php
+    }
+}
+
+if (! function_exists("import_tyrux")) {
+    function import_tyrux()
+    {
         $tyrux = assets("tyrux/index.js");
-        return '<script type="module">import "./'.$tyrux.'";</script>';
+        return '<script type="module">import "./' . $tyrux . '";</script>';
     }
 }
 
-if(! function_exists("import_bundle")){
-    function import_bundle(){
-        ?>
-        <link rel="stylesheet" href="<?=assets('code/vendor.bundle.base.css')?>" />
-        <script src="<?=assets('code/vendor.bundle.base.js')?>"></script>
-        <?php
+if (! function_exists("import_bundle")) {
+    function import_bundle()
+    {
+    ?>
+        <link rel="stylesheet" href="<?= assets('code/vendor.bundle.base.css') ?>" />
+        <script src="<?= assets('code/vendor.bundle.base.js') ?>"></script>
+    <?php
     }
 }
 
-if(! function_exists("import_datatable")){
-    function import_datatable(){
-        ?>
-        <link rel="stylesheet" href="<?=assets('code/datatable.css')?>" />
-        <script src="<?=assets('code/datatable.js')?>"></script>
-        <?php
+if (! function_exists("import_datatable")) {
+    function import_datatable()
+    {
+    ?>
+        <link rel="stylesheet" href="<?= assets('code/datatable.css') ?>" />
+        <script src="<?= assets('code/datatable.js') ?>"></script>
+    <?php
     }
 }
-if(! function_exists("import_jspost")){
-    function import_jspost(){
-        ?>
-        <script src="<?=assets('code/jspost.js')?>"></script>
-        <?php
+if (! function_exists("import_jspost")) {
+    function import_jspost()
+    {
+    ?>
+        <script src="<?= assets('code/jspost.js') ?>"></script>
+<?php
     }
 }
 
