2d6f56413bc0e8a62358a493c76c6c17fc78bc05
Tyrone <tyrone@clearmindai.com>
2025-09-16 07:54:44 +0800

    update fe date helper

--- _frontend/assets/code/date.js
+++ _frontend/assets/code/date.js
@@ -1,17 +1,24 @@
 class Date {
-    get_name($date, $format) {
+    get_name($date, $format = "F d, Y H:i:s") {
         const d = new Date($date);
+
+        let hours24 = d.getHours();
+        let hours12 = hours24 % 12 || 12;
+        let ampm = hours24 < 12 ? "AM" : "PM";
 
         const map = {
             F: d.toLocaleString("en-US", { month: "long" }), // Full month
             d: String(d.getDate()).padStart(2, "0"),         // Day with leading zero
             Y: d.getFullYear(),                              // Full year
-            H: String(d.getHours()).padStart(2, "0"),        // Hours (24)
+            H: String(hours24).padStart(2, "0"),             // 24h hour
+            h: String(hours12).padStart(2, "0"),             // 12h hour
             i: String(d.getMinutes()).padStart(2, "0"),      // Minutes
-            s: String(d.getSeconds()).padStart(2, "0")       // Seconds
+            s: String(d.getSeconds()).padStart(2, "0"),      // Seconds
+            A: ampm,                                         // AM/PM
+            a: ampm.toLowerCase()                            // am/pm
         };
 
-        return $format.replace(/F|d|Y|H|i|s/g, m => map[m]);
+        return $format.replace(/F|d|Y|H|h|i|s|A|a/g, m => map[m]);
     }
 
     change_date(date, interval) {
@@ -62,31 +69,45 @@ class Date {
     get_date(date, format = "Y-m-d H:i:s") {
         const d = new Date(date);
 
+        let hours24 = d.getHours();
+        let hours12 = hours24 % 12 || 12;
+        let ampm = hours24 < 12 ? "AM" : "PM";
+
         const map = {
             Y: d.getFullYear(),
             m: String(d.getMonth() + 1).padStart(2, "0"),
             d: String(d.getDate()).padStart(2, "0"),
-            H: String(d.getHours()).padStart(2, "0"),
+            H: String(hours24).padStart(2, "0"),
+            h: String(hours12).padStart(2, "0"),
             i: String(d.getMinutes()).padStart(2, "0"),
-            s: String(d.getSeconds()).padStart(2, "0")
+            s: String(d.getSeconds()).padStart(2, "0"),
+            A: ampm,
+            a: ampm.toLowerCase()
         };
 
-        return format.replace(/Y|m|d|H|i|s/g, m => map[m]);
+        return format.replace(/Y|m|d|H|h|i|s|A|a/g, m => map[m]);
     }
 
     now(format = "Y-m-d H:i:s") {
         const d = new Date();
 
+        let hours24 = d.getHours();
+        let hours12 = hours24 % 12 || 12;
+        let ampm = hours24 < 12 ? "AM" : "PM";
+
         const map = {
             Y: d.getFullYear(),
             m: String(d.getMonth() + 1).padStart(2, "0"),
             d: String(d.getDate()).padStart(2, "0"),
-            H: String(d.getHours()).padStart(2, "0"),
+            H: String(hours24).padStart(2, "0"),
+            h: String(hours12).padStart(2, "0"),
             i: String(d.getMinutes()).padStart(2, "0"),
-            s: String(d.getSeconds()).padStart(2, "0")
+            s: String(d.getSeconds()).padStart(2, "0"),
+            A: ampm,
+            a: ampm.toLowerCase()
         };
 
-        return format.replace(/Y|m|d|H|i|s/g, m => map[m]);
+        return format.replace(/Y|m|d|H|h|i|s|A|a/g, m => map[m]);
     }
 
 
