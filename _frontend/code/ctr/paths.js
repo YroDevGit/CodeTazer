8bcc223ccb381e78c009b2b6cd93d8f30dd5d459
Tyrone <tyrone@clearmindai.com>
2025-09-20 15:34:46 +0800

    new path

--- _frontend/code/ctr/paths.js
+++ _frontend/code/ctr/paths.js
@@ -7,40 +7,34 @@ class Path {
     }
 
     page($page = "", params = {}) {
-        let url = this.frontend + encodeURIComponent($page);
-
-        const query = Object.entries(params)
-            .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
-            .join("&");
-
-        if (query) {
-            url += "&" + query;
+        let url = this.frontend + $page;
+        if (typeof params === "object" && Object.keys(params).length > 0) {
+            const query = Object.entries(params)
+                .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+                .join("&");
+            url += "?" + query;
         }
-
         return url;
     }
 
     funcpage($page = "", params = {}) {
-        let url = this.func + encodeURIComponent($page);
-        const query = Object.entries(params)
-            .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
-            .join("&");
-
-        if (query) {
-            url += "&" + query;
+        let url = this.func + $page;
+        if (typeof params === "object" && Object.keys(params).length > 0) {
+            const query = Object.entries(params)
+                .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+                .join("&");
+            url += (url.includes("?") ? "&" : "&") + query;
         }
-
         return url;
     }
 
     backend($be = "", params = {}) {
-        let url = this.backend + encodeURIComponent($be);
-        const query = Object.entries(params)
-            .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
-            .join("&");
-
-        if (query) {
-            url += "&" + query;
+        let url = this.backend + $be;
+        if (typeof params === "object" && Object.keys(params).length > 0) {
+            const query = Object.entries(params)
+                .map(([k, v]) => `${encodeURIComponent(k)}=${encodeURIComponent(v)}`)
+                .join("&");
+            url += (url.includes("?") ? "&" : "&") + query;
         }
         return url;
     }
