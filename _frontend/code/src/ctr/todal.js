992e768b9979b16b1b1646690f252d03e7371a12
Tyrone <tyrone@clearmindai.com>
2025-10-22 12:09:40 +0800

    todal form css

--- _frontend/code/src/ctr/todal.js
+++ _frontend/code/src/ctr/todal.js
@@ -59,6 +59,32 @@ class CtrTodal {
                 display: none;
                 font-family: system-ui, sans-serif;
             }
+            .todal-form-control {
+                display: block;
+                width: 100%;
+                height: calc(1.5em + .75rem + 2px);
+                font-size: 1rem;
+                font-weight: 400;
+                line-height: 1.5;
+                color: #495057;
+                background-color: #fff;
+                background-clip: padding-box;
+                border: 1px solid #ced4da;
+                border-radius: .25rem;
+                padding: 0px 8px;
+                box-sizing: border-box;
+                transition: border-color .15s ease-in-out, box-shadow .15s ease-in-out;
+            }
+        
+            .todal-form-control:focus {
+                border-color: #80bdff;
+                outline: 0;
+                box-shadow: 0 0 0 .2rem rgba(0, 123, 255, .25);
+            }
+        
+            .todal-form-group {
+                margin-bottom: 0.7rem;
+            }
             .todal-show {
                 display: block;
                 opacity: 1;
