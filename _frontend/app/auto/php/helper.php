c10002caf48ea38f37852b56c0637a81ca450712
Tyrone <tyrone@clearmindai.com>
2025-12-18 09:48:20 +0800

    new app 9.5.7

--- _frontend/app/auto/php/helper.php
+++ _frontend/app/auto/php/helper.php
@@ -1,23 +1,39 @@
 <?php
 
-defined("ctrc") || define("ctrc", $_GET['ctrc'] ?? null);
+defined("ctrc") || define("ctrc", $_GET['ctrc'] ?? $_GET['page'] ?? null);
 
-if(! function_exists("ctr_content")){
-    function ctr_content(string $content, array|string $query = []){
+if (! function_exists("ctr_content")) {
+    function ctr_content(string $content, array|string $query = [])
+    {
         $params = "";
-        if(is_array($query)){
+        if (is_array($query)) {
             $arr = [];
-            foreach($query as $k=>$v){
-                $arr[] = $k."=".$v;
+            foreach ($query as $k => $v) {
+                $arr[] = $k . "=" . $v;
             }
             $params = implode("&", $arr);
             $params = "&" . $params;
-        }else{
-            $params = "&". $params;
+        } else {
+            $params = "&" . $params;
         }
-        return "?ctrc=". $content . $params;
+        if (! $query) {
+            $params = "";
+        }
+        $get = $_GET;
+        $arr = [];
+        $existing = "";
+        foreach ($get as $k => $v) {
+            if ($k == "ctrc" || $k == "page") {
+                continue;
+            }
+            $arr[] = $k . "=" . $v;
+            $existing = implode("&", $arr);
+            $existing = "&" . $existing;
+        }
+
+        $pg = isset($_GET['page']) ? "page=" . $_GET['page'] . "&" : "";
+        return "?" . $pg . "ctrc=" . $content . $params . $existing;
     }
 }
 
 // add more functions here...
-
